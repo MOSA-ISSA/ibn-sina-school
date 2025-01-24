50ab573ce5c47cdd1d4712681c93ff1ce19a37e4
MOSA-ISSA <124443901+MOSA-ISSA@users.noreply.github.com>
2025-01-24 02:47:59 +0200

    _

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,6 @@
 import App from "./index";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar, StyleSheet, } from "react-native";
-import { useState } from "react";
-
 export default function RootLayout() {
 
   // const [Bar, setBar] = useState("#000")
@@ -13,12 +11,8 @@ export default function RootLayout() {
         <StatusBar
           animated={true}
           backgroundColor={"#000"}
-        // barStyle={statusBarStyle}
-        // showHideTransition={statusBarTransition}
-        // hidden={hidden}
         />
-        {/* <Stack screenOptions={{ headerShown: false }} /> */}
-        <App/>
+        <App />
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -29,107 +23,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
-
-// import React, { useState } from 'react';
-// import {
-//   Button,
-//   Platform,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   View,
-// } from 'react-native';
-// import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-
-// const STYLES = ['default', 'dark-content', 'light-content'];
-// const TRANSITIONS = ['fade', 'slide', 'none'];
-
-// const App = () => {
-//   const [hidden, setHidden] = useState(false);
-//   const [statusBarStyle, setStatusBarStyle] = useState(STYLES[0]);
-//   const [statusBarTransition, setStatusBarTransition] = useState(
-//     TRANSITIONS[0],
-//   );
-
-//   const changeStatusBarVisibility = () => setHidden(!hidden);
-
-//   const changeStatusBarStyle = () => {
-//     const styleId = STYLES.indexOf(statusBarStyle) + 1;
-//     if (styleId === STYLES.length) {
-//       setStatusBarStyle(STYLES[0]);
-//     } else {
-//       setStatusBarStyle(STYLES[styleId]);
-//     }
-//   };
-
-//   const changeStatusBarTransition = () => {
-//     const transition = TRANSITIONS.indexOf(statusBarTransition) + 1;
-//     if (transition === TRANSITIONS.length) {
-//       setStatusBarTransition(TRANSITIONS[0]);
-//     } else {
-//       setStatusBarTransition(TRANSITIONS[transition]);
-//     }
-//   };
-
-//   return (
-//     <SafeAreaProvider>
-//       <SafeAreaView style={styles.container}>
-//         <StatusBar
-//           animated={true}
-//           backgroundColor="#61dafb"
-//           barStyle={statusBarStyle}
-//           showHideTransition={statusBarTransition}
-//           hidden={hidden}
-//         />
-//         <Text style={styles.textStyle}>
-//           StatusBar Visibility:{'\n'}
-//           {hidden ? 'Hidden' : 'Visible'}
-//         </Text>
-//         <Text style={styles.textStyle}>
-//           StatusBar Style:{'\n'}
-//           {statusBarStyle}
-//         </Text>
-//         {Platform.OS === 'ios' ? (
-//           <Text style={styles.textStyle}>
-//             StatusBar Transition:{'\n'}
-//             {statusBarTransition}
-//           </Text>
-//         ) : null}
-//         <View style={styles.buttonsContainer}>
-//           <Button
-//             title="Toggle StatusBar"
-//             onPress={changeStatusBarVisibility}
-//           />
-//           <Button
-//             title="Change StatusBar Style"
-//             onPress={changeStatusBarStyle}
-//           />
-//           {Platform.OS === 'ios' ? (
-//             <Button
-//               title="Change StatusBar Transition"
-//               onPress={changeStatusBarTransition}
-//             />
-//           ) : null}
-//         </View>
-//       </SafeAreaView>
-//     </SafeAreaProvider>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     backgroundColor: '#ECF0F1',
-//   },
-//   buttonsContainer: {
-//     padding: 10,
-//   },
-//   textStyle: {
-//     textAlign: 'center',
-//     marginBottom: 8,
-//   },
-// });
-
-// export default App;
